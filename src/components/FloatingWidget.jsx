5899a571c313c731b830d64e3efd40d3463a9fd3
Eva254-ke <evanszachariah36@gmail.com>
2025-07-18 23:06:00 +0300

    contact

--- src/components/FloatingWidget.jsx
+++ src/components/FloatingWidget.jsx
@@ -5,7 +5,7 @@ import whatsappIcon from '../assets/whatsapp-icon.png';
 const FloatingWidget = () => {
     return (
         <div className="floating-widget">
-            <a href="https://wa.me/0764792453" target="_blank" rel="noopener noreferrer">
+            <a href="https://wa.me/254748082746" target="_blank" rel="noopener noreferrer">
                 <button className="widget-button">
                     <img src={whatsappIcon} alt="WhatsApp" />
                     Hi, how can we help?
