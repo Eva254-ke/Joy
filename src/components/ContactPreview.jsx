387233c4ae22ce28abab2835042c230961b1861e
Eva254-ke <evanszachariah36@gmail.com>
2025-07-19 21:12:49 +0300

    location

--- src/components/ContactPreview.jsx
+++ src/components/ContactPreview.jsx
@@ -114,8 +114,8 @@ export default function ContactSection() {
               title="JoyBeauty Location in Kilimani"
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1994.396813600862!2d36.7803434!3d-1.2966743!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f10ed6a9a3897%3A0xe99c624de873c469!2sKilimani%2C%20Nairobi!5e0!3m2!1sen!2ske!4v1715588891216!5m2!1sen!2ske"
               width="100%"
-              height="100%"
-              style={{ border: 0 }}
+              height="300"
+              style={{ border: 0, minHeight: '300px', maxHeight: '400px', width: '100%' }}
               allowFullScreen
               loading="lazy"
               referrerPolicy="no-referrer-when-downgrade"
